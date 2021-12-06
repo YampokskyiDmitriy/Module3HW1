74fbea55cc46c1609ece01d7d3136d88687dd801
Dima <yadima2001@gmail.com>
2021-12-06 17:02:14 +0200

    list

---
+++ Module3HW1/Starter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Module3HW1.Helpers;
+
+namespace Module3HW1
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            var arrayInit = new[]
+            {
+                4,
+                5,
+                6,
+                1,
+                2,
+            };
+            var myList = new MyList<int>(arrayInit);
+            var itemToRemove = 1;
+            myList.Remove(itemToRemove);
+            myList.RemoveAtIndex(3);
+            myList.Add(10);
+            myList.Add(10);
+            myList.Add(10);
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            myList.Sort(new MyListComparer<int>());
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"capacity {myList.Capacity}");
+        }
+    }
+}
